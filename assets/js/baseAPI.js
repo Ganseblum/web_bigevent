df1192b1a7ff1b6ae01cbcf1113980da9c7a0fe1
Ganseblum <977252487@qq.com>
2022-04-24 19:47:27 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,23 @@
+  
+  // 在发起ajax请求之前，回先调用ajaxPrefilter这个函数
+  
   $.ajaxPrefilter(function(options){
+    // 在发起ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    // 统一未有权限的结构，设置请求头
+    if(options.url.indexOf('/my/') !== -1){
+      options.headers = {
+        Authorization: localStorage.getItem('token') || ''
+      }
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function(res){
+      if(res.responseJSON.status ===1 || res.responseJSON.status === "身份认证失败！"){
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+        console.log(res.responseJSON);
+        } 
+    }
   })
---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+$(function(){
+  getUerSugget()
+
+  $('.goBack').on('click',function(){
+      var layer = layui.layer
+    layer.confirm('确定要退出吗?', {icon: 3, title:'提示'}, function(index){
+      //do something
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+      layer.close(index);
+    });
+  })
+
+
+})
+
+function getUerSugget(){
+  $.ajax({
+    method: "GET",
+    url: '/my/userinfo',
+    // headers:{
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
+    success: function(res){
+      if(res.status !== 0){
+        return layer.msg('获取失败')
+      }
+      renderAvatar(res.data)
+
+    },
+    // complete: function(res){
+    //   // console.log(res);
+    //   if(res.responseJSON.status ===1 || res.responseJSON.status === "身份认证失败！"){
+    //     localStorage.removeItem('token')
+    //     location.href = '/login.html'
+    //     console.log(res.responseJSON);
+    //     } 
+    // }
+  })
+}
+
+function renderAvatar(data){
+  if(data.user_pic !== null){
+    $('.layui-nav-img').attr('src','res.data.user_pic').show()
+    $('.text-useravants').hide()
+  }
+  $('.layui-nav-img').attr('src','res.data.user_pic').hide()
+    var name = data.username
+    $(".welcome").html('欢迎&nbsp;&nbsp;' + name)
+    var first = name[0].toUpperCase()
+    $('.text-useravants').html(first).show()
+}
+
